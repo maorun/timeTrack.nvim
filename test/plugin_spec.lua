662f3da8b9a5065a5ed6eecda57c9e3382e429b9
Marco Driemel <marco.driemel@axelspringer.com>
2024-03-18 18:04:40 +0100

    test(plugin_spec): Add test cases for maoruntime functions
    Add test cases for TimeStart, TimeStop, TimePause, and TimeResume functions
    in the maoruntime plugin. This helps ensure the functionality and reliability
    of these key time-related functions in the plugin.

---
+++ test/plugin_spec.lua
@@ -0,0 +1,31 @@
+local maorunTime = require "maorun.time"
+local os = require "os"
+
+TestMaorunTime = {}
+
+local tempPath = os.tmpname()
+
+function TestMaorunTime:setUp()
+    maorunTime.setup(tempPath)
+end
+
+function TestMaorunTime:testTimeStart()
+    maorunTime.TimeStart()
+    assert.is_false(maorunTime.isPaused())
+end
+
+function TestMaorunTime:testTimeStop()
+    maorunTime.TimeStop()
+    assert.is_true(maorunTime.isPaused())
+end
+
+function TestMaorunTime:testTimePause()
+    maorunTime.TimePause()
+    assert.is_true(maorunTime.isPaused())
+end
+
+function TestMaorunTime:testTimeResume()
+    maorunTime.TimeResume()
+    assert.is_false(maorunTime.isPaused())
+end
+
